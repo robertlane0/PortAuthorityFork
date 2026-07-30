3609cbf4e440326f73220de3aae2ff736dd14127
robertlane0 <robertlane0@protonmail.com>
2026-07-30 10:42:05 -0700

    DeepSeek V4 Flash: changed IP API to icanhazip.com

--- app/src/main/java/com/aaronjwood/portauthority/async/WanIpAsyncTask.java
+++ app/src/main/java/com/aaronjwood/portauthority/async/WanIpAsyncTask.java
@@ -18,8 +18,7 @@ import okhttp3.ResponseBody;
 
 public class WanIpAsyncTask extends AsyncTask<Void, Void, String> {
 
-    // IP service is 100% open source https://github.com/aaronjwood/public-ip-api
-    private static final String EXTERNAL_IP_SERVICE = "https://yourip.aaronjwood.com/";
+    private static final String EXTERNAL_IP_SERVICE = "https://icanhazip.com/";
     private final WeakReference<MainAsyncResponse> delegate;
 
     /**
